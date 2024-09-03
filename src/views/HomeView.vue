807d5a683ca13ca34bbed2a67a262ea83d863d29
ZaynOssama <oussamazazou@iesmontsia.org>
2024-09-03 17:49:36 +0200

    Initial Files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="container">
+    <div class="row">
+      <div class="title">
+        WELCOME TO FC 24 PLAYER INFO
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
